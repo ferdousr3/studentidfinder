5be45f73aae806d24e99a4db62fe321fff97f34f
ferdousr3 <ferdousr1992@gmail.com>
2021-04-15 12:56:41 +0600

    upload my first calculator

---
+++ cal.js
@@ -0,0 +1,13 @@
+let num1 = prompt("Input Your First Number");
+let num2 = prompt("Input Your Secound Number");
+
+
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+result = num1 + num2;
+document.write("Addition : " + num1 + " + " + num2 + " = " + result + "<br/>" );
+
+result = num1 - num2;
+document.write( "Substration : " + num1 + " - " + num2 + " = " + result);

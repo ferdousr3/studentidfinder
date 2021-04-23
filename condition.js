2ea6425f29f53c231e5ec6b8ab8248be8865a877
ferdousr3 <ferdousr1992@gmail.com>
2021-04-23 11:23:41 +0600

    project

---
+++ condition.js
@@ -0,0 +1,47 @@
+// let haveMoney = 6000;
+
+// if(haveMoney == 5000) {
+//     console.log("Building Purchase");
+// }
+// else if(hoveMoney > 5000){
+//     console.log("wow you buy");
+// }
+// else{
+//     console.log("not buy");
+// }
+// let age = 60;
+
+// if(age <= 17){
+//     console.log("you can wathch funny video");
+// }
+// else if(age >=18 && age <= 50){
+//     console.log("you can watch romantich video");
+
+// }
+// else{
+//     console.log("you shoul watch robidrao songit");
+// };
+
+// let num = 6 ;
+
+
+// switch(num){
+//     case 1:
+//     console.log('one');
+//     break;
+
+//     case 2:
+//     console.log('two');
+//     break;
+
+//     case 3:
+//     console.log('three');
+//     break;
+
+//     case 4:
+//     console.log('four');
+//     break;
+
+//     default:
+//     console.log('please inter the value between 1 to 4 ');
+// }
